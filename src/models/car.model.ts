f6eee72e78097ae569895bf803f248e756c7dabe
jeberlein3 <jeberlein3@mail.fresnostate.edu>
2019-05-05 15:32:49 -0700

    get methods for cert,car,and profile

---
+++ src/models/car.model.ts
@@ -0,0 +1,77 @@
+import { Request, Response, NextFunction } from 'express';
+
+export class Car {
+    _model: any;
+    constructor(norm: any) {
+      this.model = [{
+        car_id: { type: Number, key: 'primary' },
+        car_make: { type: String, maxlength: 24 },
+        car_model: {type: String, maxLength: 24},
+        car_mileage: {type:Number},
+        car_year: {type: Number},
+        user_id: {
+          type: Number,
+          key: 'foreign',
+          references: { table: 'User', foreignKey: 'id' },
+          onDelete: 'cascade',
+          onUpdate: 'cascade'
+        },
+      }, 'A table to store certifications model', [
+        {
+        route: '/getCar',
+        method: 'POST',
+        callback: this.getCarFunc,
+        requireToken: true,
+    },
+    {
+      route: '/getCarbyID/:car_id',
+      method: 'POST',
+      callback: this.getCarFuncbyID,
+      requireToken: true,
+    }
+
+  
+  
+  
+  ]];}
+      
+    getCarFunc(model: any) {
+      return async(req: Request, res: Response, next: NextFunction) => {
+          req.body= {
+                  get: ["*"]
+              }
+          let carModel = model.controller;
+          //console.log('model.model.controller', model.model.controller);
+          //console.log('model get',certsModel.get)
+          let resp = carModel.get(req, null, null);
+          console.log('from test model resp:', resp);
+          res.json({ message: 'getCertifications works...' });
+      }
+  }
+  getCarFuncbyID(model: any) {
+    return async(req: Request, res: Response, next: NextFunction) => {
+        req.body= {
+                get: ["*"],
+                where: {
+                  cert_id:req.params.id
+                }
+            }
+        let carModel = model.controller;
+        //console.log('model.model.controller', model.model.controller);
+        //console.log('model get',certsModel.get)
+        let resp = carModel.get(req, null, null);
+        console.log('from test model resp:', resp);
+        res.json({ message: 'getCertifications works...' });
+    }
+}
+  
+    set model(model: any) {
+      this.model = model;
+    }
+  
+    get model() {
+      return this.model;
+    }
+
+
+}
--- src/models/profile.model.ts
+++ src/models/profile.model.ts
@@ -1,4 +1,5 @@
 import { link } from "fs";
+import { Request, Response, NextFunction } from 'express';
 
 export class Profile {
     _model: any;
@@ -17,7 +18,54 @@ export class Profile {
           onDelete: 'cascade',
           onUpdate: 'cascade'
         },
-      }, 'A table to store exam model', []];
+      }, 'A table to store exam model', [
+        {
+          route: '/getProfile',
+          method: 'POST',
+          callback: this.getProfileFunc,
+          requireToken: true,
+      },
+      {
+        route: '/getProfilebyID/:profile_id',
+        method: 'POST',
+        callback: this.getProfileFuncbyID,
+        requireToken: true,
+      }
+  
+    
+    
+    
+    ]];}
+        
+      getProfileFunc(model: any) {
+        return async(req: Request, res: Response, next: NextFunction) => {
+            req.body= {
+                    get: ["*"]
+                }
+            let profileModel = model.controller;
+            //console.log('model.model.controller', model.model.controller);
+            //console.log('model get',certsModel.get)
+            let resp = profileModel.get(req, null, null);
+            console.log('from test model resp:', resp);
+            res.json({ message: 'getCertifications works...' });
+        }
+    }
+    getProfileFuncbyID(model: any) {
+      return async(req: Request, res: Response, next: NextFunction) => {
+          req.body= {
+                  get: ["*"],
+                  where: {
+                    cert_id:req.params.id
+                  }
+              }
+          let profileModel = model.controller;
+          //console.log('model.model.controller', model.model.controller);
+          //console.log('model get',certsModel.get)
+          let resp = profileModel.get(req, null, null);
+          console.log('from profile model resp:', resp);
+          res.json({ message: 'getProfile works...' });
+      }
+      ;
     }
   
     set model(model: any) {
